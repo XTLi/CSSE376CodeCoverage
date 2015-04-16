cb678a3455c341641c1c5c80386acac087950124
XTLi <xtli1689@163.com>
2015-04-16 17:41:50 -0400

    Task4: Revisiting Discount. Achieve 100% coverage.

--- src/ExpediaTest/FlightTest.java
+++ src/ExpediaTest/FlightTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import Expedia.*;
 
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
--- src/ExpediaTest/HotelTest.java
+++ src/ExpediaTest/HotelTest.java
@@ -113,4 +113,5 @@ public class HotelTest
 //
 //        mocks.VerifyAll();
 //    }
+	
 }
